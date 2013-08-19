9d4ade5d3d6c5571c0f85cb7549cb68a020e269e
Changbeom Ahn <kewlbear@gmail.com>
2013-08-19 14:16:42 +0900

    Initial commit

---
+++ build-ffmpeg.sh
@@ -0,0 +1,97 @@
+#!/bin/sh
+
+CONFIGURE_FLAGS="--enable-cross-compile --disable-debug --disable-ffmpeg \
+                 --disable-ffplay --disable-ffprobe --disable-ffserver \
+                 --disable-doc --disable-encoders --disable-muxers \
+                 --disable-bsfs --disable-devices --disable-filters --enable-pic"
+
+LIBS="libavcodec libavformat libavutil libswscale libavdevice libavfilter \
+      libswresample"
+
+ARCHS="armv7 armv7s i386"
+
+# directories
+SOURCE="ffmpeg"
+FAT="fat"
+
+SCRATCH="scratch"
+# must be an absolute path
+THIN=`pwd`/"thin"
+
+COMPILE="y"
+LIPO="y"
+
+if [ "$*" ]
+then
+	if [ "$*" = "lipo" ]
+	then
+		# skip compile
+		COMPILE=
+	else
+		ARCHS="$*"
+		if [ $# -eq 1 ]
+		then
+			# skip lipo
+			LIPO=
+		fi
+	fi
+fi
+
+if [ "$COMPILE" ]
+then
+	CWD=`pwd`
+	for ARCH in $ARCHS
+	do
+		echo "building $ARCH..."
+		mkdir -p "$SCRATCH/$ARCH"
+		cd "$SCRATCH/$ARCH"
+
+		if [ "$ARCH" = "i386" ]
+		then
+		    PLATFORM="iPhoneSimulator"
+		else
+		    PLATFORM="iPhoneOS"
+		    if [ $ARCH = "armv7s" ]
+		    then
+		    	CPU="--cpu=swift"
+		    fi
+		fi
+
+		XCRUN_SDK=`echo $PLATFORM | tr '[:upper:]' '[:lower:]'`
+		CC="xcrun -sdk $XCRUN_SDK clang"
+		CFLAGS="-arch $ARCH"
+		CXXFLAGS="-arch $ARCH"
+		LDFLAGS="-arch $ARCH"
+
+		$CWD/$SOURCE/configure \
+		    --target-os=darwin \
+		    --arch=$ARCH \
+		    --cc="$CC" \
+		    $CONFIGURE_FLAGS \
+		    --extra-cflags="$CFLAGS" \
+		    --extra-cxxflags="$CXXFLAGS" \
+		    --extra-ldflags="$LDFLAGS" \
+		    $CPU \
+		    --prefix="$THIN/$ARCH"
+
+		make -j3 install
+		cd $CWD
+	done
+fi
+
+if [ "$LIPO" ]
+then
+	echo "building fat binaries..."
+	mkdir -p $FAT/lib
+	set - $ARCHS
+	CWD=`pwd`
+	cd $THIN/$1/lib
+	for LIB in *.a
+	do
+		cd $CWD
+		lipo -create `find $THIN -name $LIB` -output $FAT/lib/$LIB
+	done
+
+	cd $CWD
+	cp -rf $THIN/$1/include $FAT
+fi
