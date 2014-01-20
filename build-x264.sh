e91298ebc6940eb27950417ccd370294627a5ae2
Changbeom Ahn <kewlbear@gmail.com>
2014-01-20 14:44:43 +0900

    added build-x264.sh

---
+++ build-x264.sh
@@ -0,0 +1,102 @@
+#!/bin/sh
+
+CONFIGURE_FLAGS="--enable-static --enable-pic --disable-cli --disable-asm"
+
+ARCHS="arm64 armv7s x86_64 i386 armv7"
+
+# directories
+SOURCE="x264"
+FAT="fat-x264"
+
+SCRATCH="scratch-x264"
+# must be an absolute path
+THIN=`pwd`/"thin-x264"
+
+COMPILE="y"
+LIPO="y"
+
+if [ "$*" ]
+then
+	if [ "$*" = "lipo" ]
+	then
+		# skip compile
+		COMPILE=
+	else
+		ARCHS="$*"
+		if [ $# -eq 1 ]
+		then
+			# skip lipo
+			LIPO=
+		fi
+	fi
+fi
+
+if [ "$COMPILE" ]
+then
+	CWD=`pwd`
+	for ARCH in $ARCHS
+	do
+		echo "building $ARCH..."
+		mkdir -p "$SCRATCH/$ARCH"
+		cd "$SCRATCH/$ARCH"
+
+		if [ "$ARCH" = "i386" -o "$ARCH" = "x86_64" ]
+		then
+		    PLATFORM="iPhoneSimulator"
+		    CPU=
+		    if [ "$ARCH" = "x86_64" ]
+		    then
+		    	SIMULATOR="-mios-simulator-version-min=7.0"
+		    	HOST=
+		    else
+		    	SIMULATOR="-mios-simulator-version-min=5.0"
+			HOST="--host=i386-apple-darwin"
+		    fi
+		else
+		    PLATFORM="iPhoneOS"
+		    if [ $ARCH = "armv7s" ]
+		    then
+		    	CPU="--cpu=swift"
+		    else
+		    	CPU=
+		    fi
+		    SIMULATOR=
+		    HOST="--host=arm-apple-darwin"
+		fi
+
+		XCRUN_SDK=`echo $PLATFORM | tr '[:upper:]' '[:lower:]'`
+		CC="xcrun -sdk $XCRUN_SDK clang"
+		AS="$CWD/$SOURCE/extras/gas-preprocessor.pl $CC"
+		CFLAGS="-arch $ARCH $SIMULATOR"
+		CXXFLAGS="$CFLAGS"
+		LDFLAGS="$CFLAGS"
+
+		CC=$CC $CWD/$SOURCE/configure \
+		    $CONFIGURE_FLAGS \
+		    $HOST \
+		    $CPU \
+		    --extra-cflags="$CFLAGS" \
+		    --extra-ldflags="$LDFLAGS" \
+		    --prefix="$THIN/$ARCH"
+
+		make -j3 install
+		cd $CWD
+	done
+fi
+
+if [ "$LIPO" ]
+then
+	echo "building fat binaries..."
+	mkdir -p $FAT/lib
+	set - $ARCHS
+	CWD=`pwd`
+	cd $THIN/$1/lib
+	for LIB in *.a
+	do
+		cd $CWD
+		lipo -create `find $THIN -name $LIB` -output $FAT/lib/$LIB
+	done
+
+	cd $CWD
+	cp -rf $THIN/$1/include $FAT
+fi
